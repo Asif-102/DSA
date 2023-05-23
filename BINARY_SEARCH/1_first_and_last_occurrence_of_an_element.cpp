3ccfc849c50b06d1c4282ce23f28af0454094d98
Asif-102 <asif1zero2@gmail.com>
2023-05-24 03:46:18 +0600

    find firs occurrence of an element

---
+++ BINARY_SEARCH/1_first_and_last_occurrence_of_an_element.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {2, 4, 10, 10, 10, 18, 20};
+
+    int find = 10;
+
+    int start = 0;
+    int end = v.size()-1;
+
+    int first_occur = -1;
+
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(v[mid] == find){
+            first_occur = mid;
+            end = mid-1;
+        }else if(v[mid] > find){
+            end = mid-1;
+        }else{
+            start = mid+1;
+        }
+    }
+    cout << first_occur << '\n';
+    return 0;
+}
