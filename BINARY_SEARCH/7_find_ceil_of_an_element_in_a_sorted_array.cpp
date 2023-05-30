14fd670387b80913ba0ee05d0310bebc2405e853
Asif-102 <asif1zero2@gmail.com>
2023-05-31 01:43:39 +0600

    find ceil of an element in a sorted array

---
+++ BINARY_SEARCH/7_find_ceil_of_an_element_in_a_sorted_array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v = {1, 2, 3, 4, 8, 10, 10, 12, 19};
+
+    int N = v.size();
+
+    int start = 0;
+    int end = N-1;
+
+    int x = 5;
+
+    int res = -1;
+
+    while(start <= end){
+        int mid = start + (end-start)/2;
+        if(v[mid] == x){
+            res = v[mid];
+            break;
+        }else if(v[mid] > x){
+            res = v[mid];
+            end = mid-1;
+        }else if(v[mid] < x){
+            start = mid+1;
+        }
+    }
+    cout << res << '\n';
+    return 0;
+}
